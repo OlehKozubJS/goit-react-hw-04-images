2726927f766aa975751f08d418a8b63f5c45298f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-31 16:52:41 +0300

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,9 +1,8 @@
-import { Component } from 'react';
 import SearchbarCSS from './styles/Searchbar.module.css';
 import Icons from './images/icons.svg';
 import propTypes from 'prop-types';
 
-export class Searchbar extends Component {
+export const Searchbar = () => {
   enterSearchData = event => {
     event.preventDefault();
 
@@ -13,32 +12,27 @@ export class Searchbar extends Component {
     event.currentTarget.reset();
   };
 
-  render() {
-    return (
-      <header className={SearchbarCSS.Searchbar}>
-        <form
-          className={SearchbarCSS.SearchForm}
-          onSubmit={this.enterSearchData}
-        >
-          <button type="submit" className={SearchbarCSS.SearchFormButton}>
-            <svg className={SearchbarCSS.SearchFormButtonIcon}>
-              <use xlinkHref={`${Icons}#icon-search`}></use>
-            </svg>
-          </button>
+  return (
+    <header className={SearchbarCSS.Searchbar}>
+      <form className={SearchbarCSS.SearchForm} onSubmit={this.enterSearchData}>
+        <button type="submit" className={SearchbarCSS.SearchFormButton}>
+          <svg className={SearchbarCSS.SearchFormButtonIcon}>
+            <use xlinkHref={`${Icons}#icon-search`}></use>
+          </svg>
+        </button>
 
-          <input
-            className={SearchbarCSS.SearchFormInput}
-            name="searchInput"
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={SearchbarCSS.SearchFormInput}
+          name="searchInput"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   submitFunction: propTypes.func.isRequired,
