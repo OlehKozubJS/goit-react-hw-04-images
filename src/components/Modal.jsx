5f3fc0b197be7697a96d8521e9927498d5743c6f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-07-31 17:57:44 +0300

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import ModalCSS from './styles/Modal.module.css';
 import propTypes from 'prop-types';
 
 const useKeyDown = (event, eventFunction) => {
-  if (event.key === 'Escape') {
-    eventFunction();
-  }
+  const [isOpen, setIsOpen] = useState(true);
+  const close = () => {
+    if (event.key === 'Escape') {
+      setIsOpen(false);
+    }
+  };
+  return { isOpen, close };
 };
 
 export const Modal = ({ eventFunction, imageLink }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const { isOpen, close } = useKeyDown;
 
   const handleClick = event => {
     if (event.target.className === ModalCSS.Overlay) {
@@ -17,24 +21,19 @@ export const Modal = ({ eventFunction, imageLink }) => {
     }
   };
 
-  useEffect(
-    () => document.addEventListener('keydown', handleKeyDown.bind(this)),
-    []
-  );
+  useEffect(() => document.addEventListener('keydown', close.bind(this)), []);
 
   useEffect(
-    () => document.removeEventListener('keydown', handleKeyDown.bind(this)),
-    [eventFunction]
+    () => document.removeEventListener('keydown', close.bind(this)),
+    [close]
   );
 
   return (
-    isOpen && (
-      <div onClick={handleClick} className={ModalCSS.Overlay}>
-        <div className={ModalCSS.Modal}>
-          <img src={imageLink} alt="" />
-        </div>
+    <div onClick={handleClick} className={ModalCSS.Overlay}>
+      <div className={ModalCSS.Modal}>
+        <img src={imageLink} alt="" />
       </div>
-    )
+    </div>
   );
 };
 
